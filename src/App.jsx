a45d943379951adf986356f3ffa57981b101fec2
Stayhappy-web <igboanugoemmanuelchidera@gmail.com>
2026-06-07 11:26:00 -0700

    fix correct VideoSection.css casing

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import FactsTable from "./components/FactsTable/FactsTable";
 import ContactForm from "./components/ContactForm/ContactForm";
 import Footer from "./components/Footer/Footer";
 
-
 function App() {
   return (
     <>
